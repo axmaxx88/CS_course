e05d99bbabea4620831352b2ffdb221b10036287
axmaxx88 <ax_maxx@mail.ru>
2023-10-16 22:19:17 +0400

    Add some functions

---
+++ CS_course/Functions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_course
+{
+    internal class Functions
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, x = 1, y = 5;
+            Add(ref sum, x, y);
+            Console.WriteLine(sum);
+        }
+
+        static int summ(int h, int v)
+        {
+            return h + v;
+        }
+
+        static void Add(ref int sum, int x, int y = 8)
+        {
+            sum = x + y;
+        }
+    }
+}
