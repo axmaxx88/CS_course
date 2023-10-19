f168f176eb42368b1ddb2762e81eb4c7fba1e0eb
axmaxx88 <92793703+axmaxx88@users.noreply.github.com>
2023-10-19 09:00:32 +0300

    Update Classes.cs

--- CS_course/Classes.cs
+++ CS_course/Classes.cs
@@ -15,6 +15,9 @@ namespace CS_course
             ferrari.HorsePower = 471;
             ferrari.Age = 30;
             ferrari.MaxSpeed = 317.0f;
+
+            ferrari.ShowTechnicalPasport();
+            ferrari.BecomeOlder(5, 50);
             
         } 
     }
@@ -26,6 +29,19 @@ namespace CS_course
         public int Age;
         public float MaxSpeed;
         private int _model;
+
+        //Методы класса 
+        public void ShowTechicalPasport()
+        {
+            Console.WriteLine($"Name auto: {Name}\nHorse Power amount: {HorsePower}" + 
+                             $"\nAge of car: {Age}\nMax Speed: {MaxSpeed} ")
+        }
+
+        public void BecomeOlder(int years, int runAwayHorses)
+        {
+            Age += years;
+            HorsePower -= runAwayHorses;
+        }
     }
    
 }
